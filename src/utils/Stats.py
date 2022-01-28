c5047052dad5c2f27d6f706742a1b498ad73c154
Jesús A. Manjón <jesus.manjon@urv.cat>
2022-01-28 11:13:54 +0100

    Compute RSME

--- src/utils/Stats.py
+++ src/utils/Stats.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 from src.entities.Dataset import Dataset
 
 
@@ -19,10 +21,16 @@ class Stats:
     def get_perc_of_removed_locations(self):
         return self.get_number_of_removed_locations() / self.original_dataset.get_number_of_locations()
 
-    # def get_rsme(self):
-    #
-    #     dist = 0.0
-    #     for t1 in self.original_dataset.trajectories:
-    #         t1_anon = self.anonymized_dataset.get_trajectory(t1.id)
-    #         if t1_anon:
-    #             dist =
+    def get_rsme(self, distance):
+        # TODO: Y como se mide la diferencia cuando una trayectoría ha sido eliminada?
+        dist = 0.0
+        for t1 in self.original_dataset.trajectories:
+            t1_anon = self.anonymized_dataset.get_trajectory(t1.id)
+            if t1_anon:
+                d = distance.compute(t1, t1_anon)
+                if d and d != 9999999999999:
+                    dist += pow(d, 2)
+
+        dist = sqrt(dist) / len(self.anonymized_dataset)
+
+        return dist
