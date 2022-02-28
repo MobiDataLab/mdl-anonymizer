0deb0df9c0f1dd3a559a8a06f267bd4ebe4c98a3
Jesús A. Manjón <jesus.manjon@urv.cat>
2022-02-28 13:50:36 +0100

    Optimized maps

--- examples/other/test_micro.py
+++ examples/other/test_micro.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 import pandas
 import skmob
@@ -10,46 +11,50 @@ from src.entities.Dataset import Dataset
 from src.io.scikit import Scikit
 from src.utils.Stats import Stats
 
+def plot_cluster(cluster_id, filename="out/cluster.html"):
+    cluster = microaggregation.get_clusters()[cluster_id]
+    centroid = microaggregation.get_centroids()[cluster_id]
+
+    # Plot centroid
+    dataset = Scikit.export_trajectory(centroid)
+    tdf_centroid = skmob.TrajDataFrame(dataset, user_id=3, latitude=0, longitude=1, datetime=2)
+
+    map = tdf_centroid.plot_trajectory(max_users=1, max_points=None, zoom=12, weight=3, opacity=0.9, hex_color='#FF0000', tiles='Stamen Toner')
+
+    #Plot cluster
+    dataset = []
+    for t in cluster:
+        traj = Scikit.export_trajectory(t)
+        dataset.extend(traj)
+
+    tdf_cluster = skmob.TrajDataFrame(dataset, user_id=3, latitude=0, longitude=1, datetime=2)
+
+    map = tdf_cluster.plot_trajectory(map_f=map, max_points=None, zoom=12, weight=2, opacity=0.9, hex_color='#4ffdff',tiles='Stamen Toner')
+
+    map.save(filename)
+
+
+############################
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO)
+
 dataset = Dataset()
-dataset.load_from_scikit('../anonymize/data/cabs_dataset_20080608.csv', n_trajectories=200, min_locations=10, datetime_key="timestamp")
+dataset.load_from_scikit('../anonymize/data/cabs_dataset_20080608_0800_1200.csv', min_locations=10, datetime_key="timestamp")
+dataset.filter_by_speed()
 dataset.export_to_scikit(filename="out/actual_dataset_loaded.csv")
 
-Martinez2021_distance = Distance(dataset, landa=1.0480570490488479)
-microaggregation = Microaggregation(dataset, k=5, distance=Martinez2021_distance)
+Martinez2021_distance = Distance(dataset, landa=0.6506385836188395)
+microaggregation = Microaggregation(dataset, k=3, distance=Martinez2021_distance)
 microaggregation.run()
 
 anon_dataset = microaggregation.get_anonymized_dataset()
 anon_dataset.set_description("DATASET ANONYMIZED - MICROAGGREGATION")
 anon_dataset.export_to_scikit(filename="out/cabs_scikit_anonymized_Micro_Martinez.csv")
 
-cluster_0 = microaggregation.get_clusters()[0]
-centroid_0 = microaggregation.get_centroids()[0]
-
-# print(json.dumps(cluster_0, sort_keys=True, indent=4, default=str))
-# print(centroid_0)
-
-mini_dataset = Scikit.export_trajectory(centroid_0)
-for t in cluster_0:
-    traj = Scikit.export_trajectory(t)
-    mini_dataset.extend(traj)
-
-tdf = skmob.TrajDataFrame(mini_dataset, user_id=3, latitude=0, longitude=1, datetime=2)
-plot = tdf.plot_trajectory(zoom=12, weight=3, opacity=0.9, tiles='Stamen Toner')
-plot.save('out/first_cluster.html')
+plot_cluster(cluster_id=0, filename='out/first_cluster.html')
 
 last_cluster_id = list(microaggregation.get_centroids())[-1]
-cluster_last = microaggregation.get_clusters()[last_cluster_id]
-centroid_last = microaggregation.get_centroids()[last_cluster_id]
-
-mini_dataset = Scikit.export_trajectory(centroid_last)
-for t in cluster_last:
-    traj = Scikit.export_trajectory(t)
-    mini_dataset.extend(traj)
-
-tdf = skmob.TrajDataFrame(mini_dataset, user_id=3, latitude=0, longitude=1, datetime=2)
-plot = tdf.plot_trajectory(zoom=12, weight=3, opacity=0.9, tiles='Stamen Toner')
-plot.save('out/last_cluster.html')
-
+plot_cluster(cluster_id=last_cluster_id, filename='out/last_cluster.html')
 
 
 
