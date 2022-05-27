67be3f1aaeac82d8803416a00d9a102dc9939b99
txusmanjon@gmail.com <1tfo.Kgithub>
2022-05-27 13:01:51 +0200

    Check the spatial distance with the previous location, as well

--- utilities/Roma_Taxi_to_CSV/Roma_Taxi_to_CSV.py
+++ utilities/Roma_Taxi_to_CSV/Roma_Taxi_to_CSV.py
@@ -2,22 +2,24 @@ import csv
 import datetime
 import logging
 
+from haversine import haversine, Unit
 from tqdm import tqdm
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO)
 
 input_file = "taxi_february.txt"
-output_file = "rome_taxi_dataset_10.csv"
+output_file = "rome_taxi_dataset_all_lighter.csv"
 
 # Number of trajectories to read (None if we want all trajectories)
-n_users = 1
-timestamp_users = {}
+n_users = None
+last_user_locations = {}
 
 no_locations = 0
 
-max_timestamp = 1391210655
+max_timestamp = None
 
-min_time_locations = 10             # min time between locations (just to get less locations than the original file)
+min_time_locations = 30             # min time between locations (just to get less locations than the original file)
+min_spatial_distance = 100           # min distance (meters) between locations
 
 with open(output_file, mode="w", newline='') as output:
     writer = csv.writer(output, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -31,24 +33,30 @@ with open(output_file, mode="w", newline='') as output:
             user_id = fields[0]
 
             # Trajectories to read
-            if n_users is None or user_id in timestamp_users or len(timestamp_users) < n_users:
+            if n_users is None or user_id in last_user_locations or len(last_user_locations) < n_users:
 
-                if user_id not in timestamp_users:
-                    timestamp_users[user_id] = 0
+                if user_id not in last_user_locations:
+                    last_user_locations[user_id] = (0, 0, 0)
 
                 d = fields[1]
-                idx = d.index('.')
 
-                dt_object = datetime.datetime.fromisoformat(d[0:idx])
+                idx = d.find('.')
+                if idx != -1:
+                    dt_object = datetime.datetime.fromisoformat(d[0:idx])
+                else:
+                    idx = d.find('+')
+                    dt_object = datetime.datetime.fromisoformat(d[0:idx])
+
                 timestamp = datetime.datetime.timestamp(dt_object)
 
                 location = fields[2][6:-1].rstrip().split(" ")
-                lat = location[0]
-                long = location[1]
+                lat = float(location[0])
+                long = float(location[1])
 
-                if timestamp - timestamp_users[user_id] >= min_time_locations:
+                if timestamp - last_user_locations[user_id][2] >= min_time_locations and \
+                        haversine((lat, long), (last_user_locations[user_id][0], last_user_locations[user_id][1]), unit=Unit.METERS) >= min_spatial_distance:
                     writer.writerow([lat, long, timestamp, user_id])
-                    timestamp_users[user_id] = timestamp
+                    last_user_locations[user_id] = (lat, long, timestamp)
                     no_locations += 1
 
                 if max_timestamp and timestamp > max_timestamp:
