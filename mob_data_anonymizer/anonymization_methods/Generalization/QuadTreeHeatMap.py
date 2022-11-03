8e4fbfaa48078c3478ed729b525e5cba75f92143
BenetManzanaresSalor <benetmanzanares@gmail.com>
2022-11-03 16:30:01 +0100

    QuadTreeHeatMap: New version with sectors merging

--- mob_data_anonymizer/anonymization_methods/Generalization/QuadTreeHeatMap.py
+++ mob_data_anonymizer/anonymization_methods/Generalization/QuadTreeHeatMap.py
@@ -10,7 +10,8 @@ import numpy as np
 DEFAULT_VALUES = {
     "min_k": 5,
     "max_locations": 10,
-    "max_depth": 30
+    "max_depth": 30,
+    "merge_sectors": True
 }
 
 
@@ -19,7 +20,8 @@ class QuadTreeHeatMap:
     Similar approach to the used in https://www.idescat.cat/sort/sort411/41.1.7.lagonigro-etal.prov.pdf
     """
 
-    def __init__(self, dataset: Dataset, min_k: int, max_locations: int, max_depth: int):
+    def __init__(self, dataset: Dataset, min_k: int, max_locations: int,
+                 max_depth: int, merge_sectors: bool):
         """
         Parameters
         ----------
@@ -32,26 +34,23 @@ class QuadTreeHeatMap:
             It must be greater than min_k.
         max_depth : int
             Maximum depth allowed for the QuadTree.
+        merge_sectors : bool
+            If True, sectors with an insufficient number of locations will be merged with neighboring sectors.
+            This should improve utility.
         """
         self.dataset = dataset
         self.min_k = min_k
         self.max_locations = max_locations
         assert max_locations > min_k
         self.max_depth = max_depth
-        self.heatmap_nodes = []
-
-        self.anonymized_dataset = dataset.__class__()
+        self.merge_sectors = merge_sectors
+        print("INIT", self.merge_sectors)
+        self.heatmap_nodes = None
+        self.anonymized_dataset = None
 
     def run(self):
         # Reset heatmap and transform dataset to NumPy
-        self.heatmap_nodes = []
         np_dataset = self.dataset.to_numpy()
-
-        # Patchy filter for outliers outside San Francisco # TODO: Remove it
-        """np_dataset = np.delete(np_dataset, np.argwhere(np_dataset[:, 0] < 37.4), axis=0)
-        np_dataset = np.delete(np_dataset, np.argwhere(np_dataset[:, 0] > 37.9), axis=0)
-        np_dataset = np.delete(np_dataset, np.argwhere(np_dataset[:, 1] < -122.6), axis=0)
-        np_dataset = np.delete(np_dataset, np.argwhere(np_dataset[:, 1] > -122.0), axis=0)"""
 
         # Initialize QuadTree with bounding box
         min_x = min(np_dataset[:, 0])
@@ -87,7 +86,6 @@ class QuadTreeHeatMap:
         logging.info("Done!")
 
         # Plotting # TODO: Remove
-        # print(gdf.to_string())
         plt.scatter(np_dataset[:, 1], np_dataset[:, 0])
         fig, ax = plt.subplots(1, 1)
         gdf.plot(column='n_location', cmap='OrRd', ax=ax, legend=True)
@@ -105,96 +103,116 @@ class QuadTreeHeatMap:
                 heatmap.append((self.get_bbox(qtree_elem), n_parent_locations))
         # Otherwise, check children
         else:
-            # TODO: Maybe use this standard approach
-            for child in qtree_elem.children:
-                n_child_locations = len(child)
-                # If not enough locations, ONLY add *parent* to heatmap and break
-                if n_child_locations < self.min_k:
-                    n_parent_locations = len(qtree_elem)
-                    heatmap = []  # Reset heatmap of nodes from other children
-                    heatmap = [(self.get_bbox(qtree_elem), n_parent_locations)]  # Add parent
-                    break
-                # If child has enough locations, search inside
-                else:
-                    heatmap += self.qtree_to_heatmap(child)
+            # If merge sectors
+            if self.merge_sectors:
+                if qtree_elem == self.qtree: print("MERGING")
+                # Try to merge children with not enough locations
+                children_idxs = list(range(len(qtree_elem.children)))
+                children_nlocs = [len(child) for child in qtree_elem.children]
+                children_idxs_by_nlocs = [idx for _, idx in sorted(zip(children_nlocs, children_idxs))]  # Ascending
+                idx = 0
+                while idx < len(children_idxs_by_nlocs):
+                    child_idx = children_idxs_by_nlocs[idx]
+                    child = qtree_elem.children[child_idx]
+                    n_child_locations = len(child)
 
-            # TODO: Maybe use this advanced option
-            """
-            # Try to merge children with not enough locations
-            children_idxs = list(range(len(qtree_elem.children)))
-            children_nlocs = [len(child) for child in qtree_elem.children]
-            children_idxs_by_nlocs = [idx for _, idx in sorted(zip(children_nlocs, children_idxs))]  # Ascending
-            idx = 0
-            while idx < len(children_idxs_by_nlocs):
-                child_idx = children_idxs_by_nlocs[idx]
-                child = qtree_elem.children[child_idx]
-                n_child_locations = len(child)
+                    # If no locations, ignore
+                    if n_child_locations == 0:
+                        # Avoid mergings with it
+                        children_idxs_by_nlocs.remove(child_idx)
+                        idx -= 1
+                    # If not enough locations, try to merge with neighbours
+                    elif n_child_locations < self.min_k:
+                        impossible_k_anonymity = False
 
-                # If not enough locations, try to merge with neighbours
-                if n_child_locations < self.min_k:
-                    impossible_k_anonymity = False
+                        # Get neighbours indexes
+                        # Children order: Bottom left, top left, bottom right, top right. So neightbours are at | +1,+2 | +2,+3 | +1,+2 | +2,+3
+                        if child_idx % 2 == 0:  # If pair
+                            first_neigh = (child_idx + 1) % 4
+                            second_neigh = (child_idx + 2) % 4
+                        else:  # If not pair
+                            first_neigh = (child_idx + 2) % 4
+                            second_neigh = (child_idx + 3) % 4
 
-                    # Get neighbours indexes
-                    # Children order: Bottom left, top left, bottom right, top right. So neightbours are at | +1,+2 | +2,+3 | +1,+2 | +2,+3
-                    if child_idx % 2 == 0:  # If pair
-                        first_neigh = (child_idx + 1) % 4
-                        second_neigh = (child_idx + 2) % 4
-                    else:  # If not pair
-                        first_neigh = (child_idx + 2) % 4
-                        second_neigh = (child_idx + 3) % 4
+                        # If there are available neighbours for merging
+                        neighbours_idxs = [neigh_idx for neigh_idx in [first_neigh, second_neigh] if
+                                           neigh_idx in children_idxs_by_nlocs]
+                        if len(neighbours_idxs) > 0:
+                            # Sort neighbours by number of locations in ascending. A heuristic for better (not optimal) utility.
+                            neighbours_nlocs = [children_nlocs[neigh_idx] for neigh_idx in neighbours_idxs]
+                            neighbours_by_nlocs = [neigh_idx for _, neigh_idx in
+                                                   sorted(zip(neighbours_nlocs, neighbours_idxs))]
 
-                    # If there are available neighbours for merging
-                    neighbours_idxs = [neigh_idx for neigh_idx in [first_neigh, second_neigh] if neigh_idx in children_idxs_by_nlocs]
-                    if len(neighbours_idxs) > 0:
-                        # Sort neighbours by number of locations in ascending. A heuristic for better (not optimal) utility.
-                        neighbours_nlocs = [children_nlocs[neigh_idx] for neigh_idx in neighbours_idxs]
-                        neighbours_by_nlocs = [neigh_idx for _, neigh_idx in sorted(zip(neighbours_nlocs, neighbours_idxs))]
+                            # Try to merge with neighbours by #locations
+                            is_merged = False
+                            for neigh_idx in neighbours_by_nlocs:
+                                # If it #locations is enough, create *group* and add to heatmap
+                                total_locations = n_child_locations + children_nlocs[neigh_idx]
+                                if total_locations >= self.min_k:
+                                    neighbour = qtree_elem.children[neigh_idx]
+                                    bbox_1 = self.get_bbox(child)
+                                    bbox_2 = self.get_bbox(neighbour)
+                                    bbox = (min(bbox_1[0], bbox_2[0]),
+                                            min(bbox_1[1], bbox_2[1]),
+                                            max(bbox_1[2], bbox_2[2]),
+                                            max(bbox_1[3], bbox_2[3]))
+                                    heatmap.append((bbox, total_locations))
 
-                        # Try to merge with neighbours by #locations
-                        is_merged = False
-                        for neigh_idx in neighbours_by_nlocs:
-                            # If it #locations is enough, create *group* and add to heatmap
-                            total_locations = n_child_locations + children_nlocs[neigh_idx]
-                            if total_locations >= self.min_k:
-                                neighbour = qtree_elem.children[neigh_idx]
-                                bbox_1 = self.get_bbox(child)
-                                bbox_2 = self.get_bbox(neighbour)
+                                    # Avoid repeating mergings
+                                    children_idxs_by_nlocs.remove(child_idx)
+                                    children_idxs_by_nlocs.remove(neigh_idx)
+                                    idx -= 1
 
-                                # Avoid repeating mergings
-                                children_idxs_by_nlocs.remove(child_idx)
-                                children_idxs_by_nlocs.remove(neigh_idx)
-                                idx -= 1
+                                    # End of loop
+                                    is_merged = True
+                                    break
 
-                                # End of loop
-                                is_merged = True
-                                break
+                            # If it has not been merged
+                            if not is_merged:
+                                impossible_k_anonymity = True
 
-                        # If it has not been merged
-                        if not is_merged:
+                        # Otherwise, there is no merging possible
+                        else:
                             impossible_k_anonymity = True
 
-                    # Otherwise, there is no merging possible
+                        # If there is not merging that ensures k-anonymity, add *parent* to heatmap and break
+                        if impossible_k_anonymity:
+                            n_parent_locations = len(qtree_elem)
+                            heatmap = []  # Reset heatmap
+                            heatmap = [(self.get_bbox(qtree_elem), n_parent_locations)]  # Add parent
+                            break
+
+                    # If child has enough locations, search inside
                     else:
-                        impossible_k_anonymity = True
+                        heatmap += self.qtree_to_heatmap(child)
 
-                    # If there is not merging that ensures k-anonymity, add *parent* to heatmap and break
-                    if impossible_k_anonymity:
-                        n_parent_locations = len(qtree_elem)
-                        heatmap = []    # Reset heatmap
-                        heatmap = [(self.get_bbox(qtree_elem), n_parent_locations)]  # Add parent
-                        break
+                        # Avoid mergings with it
+                        children_idxs_by_nlocs.remove(child_idx)
+                        idx -= 1
 
-                # If child has enough locations, search inside
-                else:
-                    heatmap += self.qtree_to_heatmap(child)
+                    # Increment index
+                    idx += 1
+            # If no merge sectors
+            else:
+                if qtree_elem == self.qtree: print("NO MERGING")
+                for child in qtree_elem.children:
+                    n_child_locations = len(child)
+                    # If not enough locations
+                    if n_child_locations < self.min_k:
+                        # If zero locations, ignore
+                        if n_child_locations == 0:
+                            pass
+                        # Otherwise, ONLY add *parent* to heatmap and break
+                        else:
+                            n_parent_locations = len(qtree_elem)
+                            heatmap = []  # Reset heatmap of nodes from other children
+                            heatmap = [(self.get_bbox(qtree_elem), n_parent_locations)]  # Add parent
+                            break
+                    # If child has enough locations, search inside
+                    else:
+                        heatmap += self.qtree_to_heatmap(child)
 
-                    # Avoid repeating mergings
-                    children_idxs_by_nlocs.remove(child_idx)
-                    idx -= 1
 
-                # Increment index
-                idx += 1
-            """
 
         return heatmap
 
@@ -216,12 +234,12 @@ class QuadTreeHeatMap:
 
     @staticmethod
     def get_instance(data):
-        required_fields = ["min_k", "max_locations", "max_depth"]
+        required_fields = ["min_k", "max_locations", "max_depth", "merge_sectors"]
         values = {}
 
         for field in required_fields:
             values[field] = data.get(field)
-            if not values[field]:
+            if values[field] is None:
                 logging.info(f"No '{field}' provided. Using {DEFAULT_VALUES[field]}.")
                 values[field] = DEFAULT_VALUES[field]
 
@@ -229,5 +247,5 @@ class QuadTreeHeatMap:
         dataset.load_from_scikit(data.get("input_file"), min_locations=1, datetime_key="timestamp")
         dataset.filter_by_speed()
 
-        return QuadTreeHeatMap(dataset, values['min_k'],
-                               values['max_locations'], values["max_depth"])
+        return QuadTreeHeatMap(dataset, values['min_k'], values['max_locations'],
+                               values["max_depth"], values["merge_sectors"])
