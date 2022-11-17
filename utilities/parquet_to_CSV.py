e3f7bcc9ba5f2df5878ac58cf7f7bdd79222e68e
txusma23 <txusmanjon@gmail.com>
2022-11-17 12:02:18 +0100

    New parquet to CSV utility

---
+++ utilities/parquet_to_CSV.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import pyarrow.parquet as pq
+
+INPUT_FILE = "../examples/out/f_CNR_Rome.parquet"
+output_file = INPUT_FILE[:-7] + "csv"
+
+df = pq.read_table(INPUT_FILE).to_pandas()
+df.to_csv(output_file)
