39c06cd7c029595a10977e6f22a353c6cb05cf5c
Sergio <donsergioyo@gmail.com>
2023-03-21 11:24:01 +0100

    Measures

--- mob_data_anonymizer/utils/Measures.py
+++ mob_data_anonymizer/utils/Measures.py
@@ -32,9 +32,9 @@ class Measures:
         # Tessellation
         bounding_box = self.__get_bounding_box(self.pre_original_tdf)
         logging.info("Tessellating original dataset")
-        self.original_tdf = spatial_tessellation(self.pre_original_tdf, "h3_tessellation", bounding_box=bounding_box)
+        self.original_tdf, _ = spatial_tessellation(self.pre_original_tdf, "h3_tessellation", bounding_box=bounding_box)
         logging.info("Tessellating anonymized dataset")
-        self.anonymized_tdf = spatial_tessellation(self.pre_anonymized_tdf, "h3_tessellation",
+        self.anonymized_tdf, _ = spatial_tessellation(self.pre_anonymized_tdf, "h3_tessellation",
                                                    bounding_box=bounding_box)
         self.output_folder = output_folder
 
