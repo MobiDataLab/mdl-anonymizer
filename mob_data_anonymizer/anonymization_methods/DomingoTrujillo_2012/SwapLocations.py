13a18a9f5146ac065a72a8a578b158e0e859d098
Jesús A. Manjón <jesus.manjon@urv.cat>
2023-07-12 17:11:40 +0200

    Swap locations with the more similar timestamps

--- mob_data_anonymizer/anonymization_methods/DomingoTrujillo_2012/SwapLocations.py
+++ mob_data_anonymizer/anonymization_methods/DomingoTrujillo_2012/SwapLocations.py
@@ -1,5 +1,6 @@
 import logging
 import random
+from statistics import mean
 
 from haversine import Unit
 
@@ -13,8 +14,8 @@ from mob_data_anonymizer.entities.Trajectory import Trajectory
 
 DEFAULT_VALUES = {
     "k": 3,
-    "R_t": 30,
-    "R_s": 1000,
+    "R_t": 15,
+    "R_s": 200,
 }
 
 class SwapLocations(AnonymizationMethodInterface):
@@ -66,56 +67,60 @@ class SwapLocations(AnonymizationMethodInterface):
 
         self.clustering_method.run(self.k)
 
+        self.clusters = self.clustering_method.get_clusters()
+        logging.debug({c: [t.id for t in trajs] for c, trajs in self.clusters.items()})
+
         logging.info(f"Clustering finished! ")
-        logging.debug(self.clustering_method.mdav_dataset.assigned_to)
+        # logging.debug(self.clustering_method.mdav_dataset.assigned_to)
 
         logging.info("Swapping locations!")
-        self.clusters = self.clustering_method.get_clusters()
-
         self.process_clusters()
 
         logging.info('Anonymization finished!')
 
     def process_clusters(self):
-        triples_swapped = []
+        swapped_locations = []
         for c in self.clusters:
 
             cluster_trajectories = self.clusters[c]
 
             # Initialize anonymized trajectories
-            anon_trajectories = list(map(lambda t: Trajectory(t.id), cluster_trajectories))
+            anon_trajectories = list(map(lambda t: Trajectory(t.id, t.user_id), cluster_trajectories))
 
             # Let T be a random trajectory in C
             T = random.choice(cluster_trajectories)
 
             # For all "unswapped" locations in T
-            unswapped_locations = [l for l in T.locations if (T.id, l.timestamp, l.x, l.y) not in triples_swapped]
-            for landa in unswapped_locations:
+            unswapped_locations = [l for l in T.locations if (T.id, l.timestamp, l.x, l.y) not in swapped_locations]
+            for loc in unswapped_locations:
 
-                # Initialize U = {landa}
-                U = [(T.id, landa)]
+                # Initialize U = {loc}
+                U = [(T.id, loc)]
 
-                # For all trajectories t_p in C with t_p != T
+                # For all trajectories T_p in C with T_p != T
                 for T_p in [traj for traj in cluster_trajectories if traj != T]:
-                    # Look for an "unswapped" triple 'l' minimizing the intra-cluster distance in U and such that:
+                    # Look for an "unswapped" location 'l' minimizing the intra-cluster distance in U and such that:
                     d = 99999999
-                    landa_p = None
+                    loc_p = None
 
                     # Check locations not swapped yet
-                    for l in [l for l in T_p.locations if (T_p.id, l.timestamp, l.x, l.y) not in triples_swapped]:
+                    for l in [l for l in T_p.locations if (T_p.id, l.timestamp, l.x, l.y) not in swapped_locations]:
 
-                        # Check temporal distance from 'landa' to 'l'
-                        if landa.temporal_distance(l) / 60 <= self.R_t:
+                        # Check temporal distance from 'loc' to 'l'
+                        if loc.temporal_distance(l) / 60 <= self.R_t:
                             # Check spatial distance from 'landa' to 'l'
-                            if 0 <= landa.spatial_distance(l, unit=Unit.METERS) <= self.R_s:
-                                # We take the location with the minimum intra-cluster distance
-                                if TimestampedLocation.compute_centroid([tuple[1] for tuple in U]).distance(l) < d:
-                                    d = landa.distance(l)
-                                    landa_p = l
+                            if 0 <= loc.spatial_distance(l, unit=Unit.METERS) <= self.R_s:
+                                # We have a candidate. We take the location with the more similar timestamp to U
+                                timestamps = [locations[1].timestamp for locations in U]
 
-                    if landa_p:
+                                # We take the location with the minimum intra-cluster distance
+                                if abs(mean(timestamps) - l.timestamp) < d:
+                                    d = loc.distance(l)
+                                    loc_p = l
+
+                    if loc_p:
                         # If landa_p exists
-                        U.append((T_p.id, landa_p))
+                        U.append((T_p.id, loc_p))
 
                 if len(U) > 1:
                     logging.debug(f'\t\tCluster {c} swapping {U}')
@@ -128,7 +133,7 @@ class SwapLocations(AnonymizationMethodInterface):
                     for tuple in U:
                         trajectory_id = tuple[0]
                         location = tuple[1]
-                        triples_swapped.append((trajectory_id, location.timestamp, location.x, location.y))
+                        swapped_locations.append((trajectory_id, location.timestamp, location.x, location.y))
 
             # Build anonymized dataset
             for T in anon_trajectories:
@@ -138,7 +143,7 @@ class SwapLocations(AnonymizationMethodInterface):
 
             logging.debug(f'\tCluster {c} processed!')
 
-        logging.info(f'{len(triples_swapped)} triples swapped!')
+        logging.info(f'{len(swapped_locations)} triples swapped!')
 
         self.anonymized_dataset.trajectories = [t for t in self.anonymized_dataset.trajectories if len(t) > 1]
         logging.info("Removed trajectories with less than 1 locations!\n")
